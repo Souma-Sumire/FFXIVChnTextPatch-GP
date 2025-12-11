5948d66bcf26b0152397a1ee6b70c431fbbcb677
Souma-Sumire <553469159@qq.com>
2025-12-11 19:35:39 +0800

    写入默认配置

--- src/main/java/name/yumao/ffxiv/chn/util/res/ConfigResource.java
+++ src/main/java/name/yumao/ffxiv/chn/util/res/ConfigResource.java
@@ -1,5 +1,6 @@
 package name.yumao.ffxiv.chn.util.res;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -11,45 +12,67 @@ import java.util.Properties;
 
 public class ConfigResource {
 	private String configPath = "/Config.properties";
-	
+
 	private InputStream configStream;
-	
+
 	private InputStreamReader configReader;
-	
+
 	private Properties props;
-	
+
 	public String getProperty(String name) {
 		return this.props.getProperty(name);
 	}
-	
+
 	public void setProperty(String name, String value) {
 		if (this.props == null)
-			this.props = new Properties(); 
+			this.props = new Properties();
 		this.props.put(name, value);
 	}
-	
+
 	public void loadConfig() throws IOException {
 		loadConfig(this.configPath);
 	}
-	
+
 	public void loadConfig(String fileName) throws IOException {
 		if (fileName.startsWith("http://") || fileName.startsWith("https://")) {
 			this.configStream = (new URL("configStream")).openStream();
 			this.props = new Properties();
 			this.props.load(this.configStream);
 		} else {
-			this.configReader = new InputStreamReader(new FileInputStream(fileName), "UTF-8");
-			this.props = new Properties();
-			this.props.load(this.configReader);
-			this.configPath = fileName;
-		} 
+			File configFile = new File(fileName);
+			if (!configFile.exists()) {
+				if (configFile.getParentFile() != null) {
+					configFile.getParentFile().mkdirs();
+				}
+				configFile.createNewFile();
+
+				// 写入默认配置
+				this.props = new Properties();
+				setProperty("GamePath", "");
+				setProperty("DLanguage", "CHS");
+				setProperty("SLanguage", "JA");
+				setProperty("ReplaText", "1");
+				setProperty("FLanguage", "CSV");
+				setProperty("ReplaFont", "1");
+				setProperty("SkipFiles", "");
+				setProperty("TransMode", "0");
+
+				this.configPath = fileName;
+				saveProperty();
+			} else {
+				this.configReader = new InputStreamReader(new FileInputStream(fileName), "UTF-8");
+				this.props = new Properties();
+				this.props.load(this.configReader);
+				this.configPath = fileName;
+			}
+		}
 	}
-	
+
 	public void saveProperty() throws IOException {
 		saveProperty(this.configPath);
 	}
-	
+
 	public void saveProperty(String fileName) throws IOException {
-		this.props.store(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8"), (String)null);
+		this.props.store(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8"), (String) null);
 	}
 }
